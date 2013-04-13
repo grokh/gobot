7a2c655421678e5a5a0081c5be30646ec95cf081
Scott Dickey <scott@grokh.com>
2013-04-13 16:45:40 -0400

    Added additional flags for full char import

--- gobot.go
+++ gobot.go
@@ -5,8 +5,11 @@ import (
 )
 
 func main() {
+	var acct = flag.String("acct", "", "Character account for initial import.")
 	var char = flag.String("char", "", "Check char, add to DB if needed, update last_seen.")
-	var lvl = flag.Int("lvl", 0, "Check char level, update DB.")
+	var lvl = flag.Int("lvl", 0, "Char level for DB update.")
+	var class = flag.String("class", "", "Character class for initial import.")
+	var race = flag.String("race", "", "Character race for initial import.")
 	var file = flag.String("import", "", "Parse file for identify stats, import to DB.")
 	flag.Parse()
 

76e3c7e593dece5aaee6c86a2e03e3499e79bf78
Scott Dickey <scott@grokh.com>
2013-04-13 15:22:37 +0000

    Switched to SQLite3

--- gobot.go
+++ gobot.go
@@ -5,9 +5,13 @@ import (
 )
 
 func main() {
-        var c = flag.String("who", "", "Check char, add to DB if needed, update last_seen.")
-        var i = flag.String("import", "", "Parse file for identify stats, import to DB.")
-        flag.Parse()
-        Who(*c)
-        ID(*i)
+	var c = flag.String("who", "", "Check char, add to DB if needed, update last_seen.")
+	var i = flag.String("import", "", "Parse file for identify stats, import to DB.")
+	flag.Parse()
+	if *c != "" {
+		Who(*c)
+	}
+	if *i != "" {
+		ID(*i)
+	}
 }
--- who.go
+++ who.go
@@ -1,48 +1,31 @@
 package main
 
 import (
-    _ "github.com/bmizerany/pq"
-    "database/sql"
-    "fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"database/sql"
+	"fmt"
 )
 
 func Who(name string) {
-	db, err := sql.Open("postgres", "user=kalkinine dbname=torildb sslmode=disable")
+	db, err := sql.Open("sqlite3", "toril.db")
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
 	defer db.Close()
 
-	sql := "SELECT account_name, char_name FROM chars WHERE LOWER(char_name) = LOWER($1)"
-	q, err := db.Prepare(sql)
-	defer q.Close()
-
-	var acc string
+	stmt, err := db.Prepare("SELECT account_name, char_name FROM chars WHERE LOWER(char_name) = LOWER(?)")
+        if err != nil {
+                fmt.Println(err)
+                return
+        }
+        defer stmt.Close()
+        var acc string
 	var char string
-	err = q.QueryRow(name).Scan(&acc, &char)
-	if err != nil {
-		panic(err)
-	}
+        err = stmt.QueryRow(name).Scan(&acc, &char)
+        if err != nil {
+                fmt.Println(err)
+                return
+        }
 	fmt.Printf("Char: %s, Acct: %s\n", acc, char)
-
-	/* HowTo: select lots of rows and iterate over all them
-	q := "SELECT account_name, char_name FROM chars WHERE LOWER(char_name) = LOWER($1)"
-	rows, err := db.Query(q, name)
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var acc string
-		var char string
-		err = rows.Scan(&acc, &char)
-		fmt.Printf("Character %s exists in the database under the account %s.\n", char, acc)
-	}
-	err = rows.Err() // get any error encountered during iteration
-	if err != nil {
-		panic(err)
-	}
-	*/
 }
-

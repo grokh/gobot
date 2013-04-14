cdf386bbb57d7df15f5571cdf857530d29fd078c
Scott Dickey <scott@grokh.com>
2013-04-14 11:15:51 -0400

    Updated comments and formatting

--- gobot.go
+++ gobot.go
@@ -5,18 +5,25 @@ import (
 )
 
 func main() {
-	var acct = flag.String("acct", "", "Character account for initial import. Ex: Krimic")
-	var char = flag.String("char", "", "Character name for DB update or import. Ex: Rynshana")
-	var lvl = flag.Int("lvl", 0, "Character level for DB update. Ex: 50")
+	// for who.go Who(char, lvl)
+	var char = flag.String("char", "", "Character name for update or import. Ex: Rynshana")
+	var lvl = flag.Int("lvl", 0, "Character level for update or import. Ex: 50")
+	// for who.go WhoChar(char, lvl, class, race, acct)
 	var class = flag.String("class", "", "Character class for initial import. Ex: \"Cleric\"")
 	var race = flag.String("race", "", "Character race for initial import. Ex: \"Moon Elf\"")
+	var acct = flag.String("acct", "", "Character account for initial import. Ex: Krimic")
+	// for identify.go Identify(filename)
 	var file = flag.String("import", "", "Parse file for identify stats, import to DB. Ex: newstats.txt")
+	// for time.go Uptime(curup)
 	var time = flag.String("time", "", "Parse uptime for boot tracking. Ex: 58:10:26")
+	// for tell.go
 	var cmd = flag.String("cmd", "", "Command from tell. Ex: stat")
 	var oper = flag.String("oper", "", "Operant from tell, to be operated on by cmd. Ex: \"a longsword\"")
+
 	flag.Parse()
 	*cmd += *oper
 
+	// only run one command at a time
 	if *char != "" && *lvl != 0 && *class != "" && *race != "" && *acct != "" {
 		WhoChar(*char, *lvl, *class, *race, *acct)
 	} else if *char != "" && *lvl != 0 {

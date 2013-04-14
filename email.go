75a1bce5ddfe921f637472eb5573053dc45f98da
Scott Dickey <scott@grokh.com>
2013-04-14 09:31:58 -0400

    Fixed email.go, working now

--- email.go
+++ email.go
@@ -9,9 +9,10 @@ func Notify(from string, to []string, pwd string) {
 	sub := "Subject: TorilMUD reboot/crash:\r\n\r\n"
 	body := "Katumi detected a new TorilMUD boot."
 	msg := []byte(sub + body)
-	server := "smtp.gmail.com:587"
+	server := "smtp.gmail.com"
+	tls := ":587"
 	auth := smtp.PlainAuth("", from, pwd, server)
-	err := smtp.SendMail(server, auth, from, to, msg)
+	err := smtp.SendMail(server+tls, auth, from, to, msg)
 	if err != nil {
 		log.Fatal(err)
 	}

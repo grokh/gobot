cff63fe565fb22ff63a1451ef7a327b986b262d1
Scott Dickey <scott@grokh.com>
2013-04-15 23:18:09 -0400

    Implemented char, class, lr, lrdel, delalt, addalt

--- tell.go
+++ tell.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
 	"log"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -241,7 +242,8 @@ func ReplyTo(char string, tell string) {
 				replied = true
 			}
 			if !replied {
-				Reply(char, NotFound("item(s)", oper))
+				txt = NotFound("item(s)", oper)
+				Reply(char, txt)
 			}
 			err = rows.Err()
 			if err != nil {
@@ -252,9 +254,9 @@ func ReplyTo(char string, tell string) {
 			Reply(char, syntax)
 		}
 	case cmd == "who" && oper != "":
-		fmt.Println()
+		//fmt.Println()
 	case cmd == "clist" && oper != "":
-		fmt.Println()
+		//fmt.Println()
 	case cmd == "char" && oper != "":
 		db, err := sql.Open("sqlite3", "toril.db")
 		if err != nil {
@@ -279,26 +281,296 @@ func ReplyTo(char string, tell string) {
 		var seen time.Time
 		err = stmt.QueryRow(oper).Scan(&lvl, &class, &name, &race, &acc, &seen)
 		if err == sql.ErrNoRows {
-			Reply(char, NotFound("character", oper))
+			txt = NotFound("character", oper)
+			Reply(char, txt)
 		} else if err != nil {
 			log.Fatal(err)
 		} else {
 			date := seen.Format("2006-01-02 15:04:05")
-			txt = "[" + lvl + " " + class + "] " + name + " (" + race + ") (@" + acc + ") seen " + date
+			txt = fmt.Sprintf(
+				"[%s %s] %s (%s) (@%s) seen %s",
+				lvl, class, name, race, acc, date,
+			)
 			Reply(char, txt)
 		}
 	case cmd == "find" && oper != "":
-		fmt.Println()
+		//fmt.Println()
 	case cmd == "class" && oper != "":
-		fmt.Println()
+		db, err := sql.Open("sqlite3", "toril.db")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
+
+		date := time.Now().Add(-time.Minute)
+		query := "SELECT char_name, class_name, char_race, char_level, account_name " +
+			"FROM chars WHERE LOWER(class_name) = LOWER(?) AND vis = 't' " +
+			"AND account_name IN " +
+			"(SELECT account_name FROM chars " +
+			"WHERE last_seen > ? " +
+			"AND vis = 't') " +
+			"ORDER BY char_level DESC"
+
+		rows, err := db.Query(query, oper, date)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		var replied bool
+		for rows.Next() {
+			var name string
+			var class string
+			var race string
+			var lvl string
+			var acct string
+			rows.Scan(&name, &class, &race, &lvl, &acct)
+			txt = fmt.Sprintf(
+				"[%s %s] %s (%s) (@%s)",
+				lvl, class, name, race, acct,
+			)
+			Reply(char, txt)
+			replied = true
+		}
+		rows.Close()
+		if !replied {
+			txt = NotFound("class", oper)
+			Reply(char, txt)
+		}
 	case cmd == "delalt" && oper != "":
-		fmt.Println()
+		db, err := sql.Open("sqlite3", "toril.db")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
+
+		query := "SELECT account_name, char_name FROM chars " +
+			"WHERE LOWER(char_name) = LOWER(?) " +
+			"AND vis = 't' AND account_name = " +
+			"(SELECT account_name FROM chars WHERE char_name = ?)"
+
+		stmt, err := db.Prepare(query)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer stmt.Close()
+
+		var acct string
+		var name string
+		err = stmt.QueryRow(oper, char).Scan(&acct, &name)
+		if err == sql.ErrNoRows {
+			txt = NotFound("character or account", oper)
+			Reply(char, txt)
+		} else if err != nil {
+			log.Fatal(err)
+		} else {
+			query = "UPDATE chars SET vis = 'f' " +
+				"WHERE LOWER(char_name) = LOWER(?)"
+
+			tx, err := db.Begin()
+			if err != nil {
+				log.Fatal(err)
+			}
+			stmt, err := tx.Prepare(query)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer stmt.Close()
+
+			_, err = stmt.Exec(oper)
+			if err != nil {
+				log.Fatal(err)
+			}
+			tx.Commit()
+			txt = fmt.Sprintf("Removed character from your alt list:: %s", oper)
+			Reply(char, txt)
+		}
 	case cmd == "addalt" && oper != "":
-		fmt.Println()
+		db, err := sql.Open("sqlite3", "toril.db")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
+
+		query := "SELECT account_name, char_name FROM chars " +
+			"WHERE LOWER(char_name) = LOWER(?) " +
+			"AND account_name = " +
+			"(SELECT account_name FROM chars WHERE char_name = ?)"
+
+		stmt, err := db.Prepare(query)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer stmt.Close()
+
+		var acct string
+		var name string
+		err = stmt.QueryRow(oper, char).Scan(&acct, &name)
+		if err == sql.ErrNoRows {
+			txt = NotFound("character or account", oper)
+			Reply(char, txt)
+		} else if err != nil {
+			log.Fatal(err)
+		} else {
+			query = "UPDATE chars SET vis = 't' " +
+				"WHERE LOWER(char_name) = LOWER(?)"
+
+			tx, err := db.Begin()
+			if err != nil {
+				log.Fatal(err)
+			}
+			stmt, err := tx.Prepare(query)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer stmt.Close()
+
+			_, err = stmt.Exec(oper)
+			if err != nil {
+				log.Fatal(err)
+			}
+			tx.Commit()
+			txt = fmt.Sprintf("Re-added character to your alt list:: %s", oper)
+			Reply(char, txt)
+		}
 	case cmd == "lr":
-		fmt.Println()
+		db, err := sql.Open("sqlite3", "toril.db")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
+
+		if oper == "" {
+			query := "SELECT char_name, report_text, " +
+				"STRFTIME('%Y-%m-%d %H:%M:%S', report_time) " +
+				"FROM loads WHERE boot_id = " +
+				"(SELECT MAX(boot_id) FROM boots)" +
+				"AND deleted = 'f'"
+
+			rows, err := db.Query(query)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer rows.Close()
+
+			var replied bool
+			counter := 1
+			for rows.Next() {
+				var name string
+				var report string
+				var rptTime time.Time
+				err = rows.Scan(&name, &report, &rptTime)
+				date := rptTime.Format("2006-01-02 05:12:19")
+				txt = fmt.Sprintf(
+					"%d: %s [%s at %s]",
+					counter, report, name, date,
+				)
+				Reply(char, txt)
+				counter++
+				replied = true
+			}
+			err = rows.Err()
+			if err != nil {
+				log.Fatal(err)
+			}
+			if !replied {
+				txt = "No loads reported for current boot."
+				Reply(char, txt)
+			}
+			rows.Close()
+		} else if strings.Contains(oper, " ") {
+			tx, err := db.Begin()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			date := time.Now()
+			query := "INSERT INTO loads " +
+				"VALUES((SELECT MAX(boot_id) FROM boots), ?, ?, ?, 'f')"
+
+			stmt, err := tx.Prepare(query)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer stmt.Close()
+
+			_, err = stmt.Exec(date, oper, char)
+			if err != nil {
+				log.Fatal(err)
+			}
+			tx.Commit()
+			txt = fmt.Sprintf("Load reported: %s", oper)
+			Reply(char, txt)
+		} else {
+			Reply(char, syntax)
+		}
 	case cmd == "lrdel" && oper != "":
-		fmt.Println()
+		db, err := sql.Open("sqlite3", "toril.db")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
+
+		query := "SELECT boot_id, report_time, report_text FROM loads " +
+			"WHERE deleted = 'f' " +
+			"AND boot_id = (SELECT MAX(boot_id) FROM boots)"
+
+		rows, err := db.Query(query)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		var curboot string
+		var rtime time.Time
+		var report string
+		num, err := strconv.Atoi(oper)
+		if err != nil {
+			Reply(char, syntax)
+			return
+		}
+		counter := 1
+		for rows.Next() {
+			var boot string
+			var date time.Time
+			var text string
+			err = rows.Scan(&boot, &date, text)
+			if counter == num {
+				curboot = boot
+				rtime = date
+				report = text
+			}
+			counter++
+		}
+		err = rows.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if counter > 1 {
+			query = "UPDATE loads SET deleted = 't' " +
+				"WHERE boot_id = ? AND report_time = ?"
+
+			tx, err := db.Begin()
+			if err != nil {
+				log.Fatal(err)
+			}
+			stmt, err := tx.Prepare(query)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer stmt.Close()
+
+			_, err = stmt.Exec(curboot, rtime)
+			if err != nil {
+				log.Fatal(err)
+			}
+			tx.Commit()
+			txt = fmt.Sprintf("Load deleted: %s", report)
+			Reply(char, txt)
+		} else {
+			txt = "No loads reported for current boot."
+			Reply(char, txt)
+		}
 	default:
 		Reply(char, syntax)
 	}

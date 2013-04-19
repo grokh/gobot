21f5ca686dbbed9c2b680f3eb5ccaa88e2ee3ef0
Scott Dickey <scott@grokh.com>
2013-04-19 00:28:41 +0000

    Simple framework for importing file for stat parsing

--- identify.go
+++ identify.go
@@ -1,5 +1,19 @@
 package main
 
-func Identify(filename string) {
+import (
+	"io/ioutil"
+	"log"
+	"strings"
+)
 
+func Identify(filename string) {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lines := strings.Split(string(content), "\n")
+
+	for _, line := range lines {
+		_ = line
+	}
 }

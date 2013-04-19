0184d8da5d6311d8ae8438c552c6e2fd690c031c
Scott Dickey <scott@grokh.com>
2013-04-19 14:34:08 -0400

    Reformat for line width, add defer close to log file

--- gobot.go
+++ gobot.go
@@ -16,34 +16,47 @@ var Char struct {
 
 func main() {
 	f, err := os.OpenFile("bot.log", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0640)
+	defer f.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.SetOutput(f)
 
 	// for who.go WhoChar(char, lvl, class, race, acct)
-	var char = flag.String("char", "", "Character name for update or import. Ex: Rynshana")
-	var lvl = flag.Int("lvl", 0, "Character level for update or import. Ex: 50")
-	var class = flag.String("class", "", "Character class for initial import. Ex: \"Cleric\"")
-	var race = flag.String("race", "", "Character race for initial import. Ex: \"Moon Elf\"")
-	var acct = flag.String("acct", "", "Character account for initial import. Ex: Krimic")
+	var char = flag.String("char", "",
+		"Character name for update or import. Ex: Rynshana")
+	var lvl = flag.Int("lvl", 0,
+		"Character level for update or import. Ex: 50")
+	var class = flag.String("class", "",
+		"Character class for initial import. Ex: \"Cleric\"")
+	var race = flag.String("race", "",
+		"Character race for initial import. Ex: \"Moon Elf\"")
+	var acct = flag.String("acct", "",
+		"Character account for initial import. Ex: Krimic")
 	// for who.go WhoBatch(ppl)
-	var who = flag.String("who", "", "Batched who output. Ex: [ 1 Ctr] Rarac  (Orc)|[ 2 War] Xatus  (Troll)")
+	var who = flag.String("who", "",
+		"Batched who output. Ex: [ 1 Ctr] Rarac  (Orc)|[ 2 War] Xatus  (Troll)")
 	// for identify.go Identify(filename)
-	var file = flag.String("import", "", "Parse file for identify stats, import to DB. Ex: newstats.txt")
+	var file = flag.String("import", "",
+		"Parse file for identify stats, import to DB. Ex: newstats.txt")
 	// for time.go Uptime(curup)
-	var time = flag.String("time", "", "Parse uptime for boot tracking. Ex: 58:10:26")
+	var time = flag.String("time", "",
+		"Parse uptime for boot tracking. Ex: 58:10:26")
 	// for tell.go ReplyTo(char, tell)
-	var tell = flag.String("tell", "", "Tell with command and maybe operant. Ex: \"stat a longsword\"")
+	var tell = flag.String("tell", "",
+		"Tell with command and maybe operant. Ex: \"stat a longsword\"")
 	// run database backup and restore
-	var backup = flag.Bool("bak", false, "Backup the toril.db database.")
-	var restore = flag.String("res", "", "Restore the toril.db database from backup file.")
+	var backup = flag.Bool("bak", false,
+		"Backup the toril.db database.")
+	var restore = flag.String("res", "",
+		"Restore the toril.db database from backup file.")
 
 	flag.Parse()
 
 	// only run one command at a time
 	switch {
-	case *char != "" && 50 >= *lvl && *lvl > 0 && *class != "" && *race != "" && *acct != "":
+	case *char != "" && 50 >= *lvl && *lvl > 0 &&
+		*class != "" && *race != "" && *acct != "":
 		WhoChar(*char, *lvl, *class, *race, *acct)
 	case *file != "":
 		Identify(*file)
@@ -54,7 +67,9 @@ func main() {
 	case *who != "":
 		WhoBatch(*who)
 	case *backup:
-		cmd := exec.Command("sh", "-c", "echo '.dump' | sqlite3 toril.db | gzip -c >toril.db.`date +\"%Y-%m-%d\"`.gz")
+		cmd := exec.Command("sh", "-c",
+			"echo '.dump' | sqlite3 toril.db | "+
+			"gzip -c >toril.db.`date +\"%Y-%m-%d\"`.gz")
 		err := cmd.Run()
 		if err != nil {
 			log.Fatal(err)

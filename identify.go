fd44a81c0f89d58d69cbc3657b0957298fd10329
Scott Dickey <scott@grokh.com>
2013-04-22 10:18:11 -0400

    Additional item parsing framework

--- identify.go
+++ identify.go
@@ -8,9 +8,17 @@ import (
 func Identify(filename string) {
 	content, err := ioutil.ReadFile(filename)
 	ChkErr(err)
-	lines := strings.Split(string(content), "\n")
 
-	for _, line := range lines {
-		_ = line
+	// do full text processing like moving stuff onto the same line
+	// split into items on double newline
+	items := strings.Split(string(content), "\n\n")
+
+	for _, item := range items {
+		// split onto separate lines for regex checking
+		lines := strings.Split(item, "\n")
+
+		for _, line := range lines {
+			_ = line
+		}
 	}
 }

c25036e02fda6410fb101e96d712ed3413a95b88
Scott Dickey <scott@grokh.com>
2013-04-24 09:57:42 -0400

    Another db function to reduce verbositalizationalizationalisty

--- db.go
+++ db.go
@@ -3,10 +3,21 @@ package main
 import (
 	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
+	"log"
 )
 
 func OpenDB() *sql.DB {
 	db, err := sql.Open("sqlite3", "toril.db")
-	ChkErr(err)
+	if err != nil {
+		log.Fatal("Fatal Error: Cannot open DB: " + err)
+	}
 	return db
+}
+
+func ChkRows(rows *sql.Rows) {
+	err := rows.Err()
+	if err != nil {
+		log.Fatal("Fatal Error: Rows returned error: " + err)
+	}
+	rows.Close()
 }

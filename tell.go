d0525d0241de1d02d80b65ce8a26a5429c66ba30
Scott Dickey <scott@grokh.com>
2013-04-24 20:13:53 -0400

    Begin functionalizing all the subsets of ReplyTo()

--- tell.go
+++ tell.go
@@ -100,6 +100,237 @@ func FindItem(oper string, length string) string {
 	return stats
 }
 
+func Help(oper string) []string {
+	oper = strings.ToLower(oper)
+	var txt []string
+	switch {
+	case oper == "?":
+		txt = append(txt, "BadSyntax: tell katumi ? -- " +
+			"Katumi provides a full listing of valid commands.")
+	case oper == "hidden?":
+		txt = append(txt, "BadSyntax: tell katumi hidden? -- " +
+			"Katumi sends a tell in reply " +
+			"if she can see you. If you receive no reply, you are hidden. " +
+			"Katumi has permanent detect invis to ensure that won't cause " +
+			"issues.")
+	case oper == "who":
+		txt = append(txt, "BadSyntax: tell katumi who <acct/char> -- " +
+			"Example: tell katumi who rynshana -- " +
+			"Katumi provides the account name along with a list of every " +
+			"known alt of the named character as a reply. Also works with " +
+			"account names.")
+	case oper == "char":
+		txt = append(txt, "BadSyntax: tell katumi char <char> -- " +
+			"Example: tell katumi char rynshana -- " +
+			"Katumi provides the account name along with full information " +
+			"on the named character as a reply, " +
+			"to include level, class, race, and date/time last seen.")
+	case oper == "find":
+		txt = append(txt, "BadSyntax: tell katumi find <acct/char> -- " +
+			"Example: tell katumi find rynshana -- " +
+			"Katumi provides the account name along with the last known " +
+			"sighting of any of that character's alts. " +
+			"If they have an alt online, " +
+			"the time will measure in seconds. " +
+			"Also works with account names.")
+	case oper == "clist":
+		txt = append(txt, "BadSyntax: tell katumi clist <acct/char> -- " +
+			"Example: tell katumi clist rynshana -- " +
+			"Katumi provides a full " +
+			"listing of every known alt belonging to <char>, including race, " +
+			"class, level, and date/time last seen, matching the format of " +
+			"the 'char' command. Also works with account names.")
+	case oper == "class":
+		txt = append(txt, "BadSyntax: tell katumi class <class> -- " +
+			"Example: tell katumi class enchanter -- " +
+			"Katumi provides a " +
+			"list of alts matching the named class for characters who " +
+			"are currently online, letting group leaders find useful " +
+			"alts from the 'who' list.")
+	case oper == "delalt":
+		txt = append(txt, "BadSyntax: tell katumi delalt <char> -- " +
+			"Example: tell katumi delalt rynshana -- " +
+			"Katumi no longer " +
+			"provides information on the alt, removing it from 'clist', " +
+			"'who', and 'find' commands. Only works for characters " +
+			"attached to the same account requesting the removal.")
+	case oper == "addalt":
+		txt = append(txt, "BadSyntax: tell katumi addalt <char> -- " +
+			"Example: tell katumi addalt rynshana -- " +
+			"Katumi begins " +
+			"providing information on the named alt, who had previously " +
+			"been removed with 'delalt', adding the character back to " +
+			"'clist', 'who', and 'find' commands. Only works for chars " +
+			"attached to the same account.")
+	case oper == "lr":
+		txt = append(txt, "BadSyntax: tell katumi lr -- " +
+			"Katumi provides a list of load " +
+			"reports for the current boot. This could be rares or quests " +
+			"other players have found or completed. Use the 'lrdel' command " +
+			"to remove bad or out of date reports.")
+		txt = append(txt, "BadSyntax: tell katumi lr <report> -- " +
+			"Example: tell katumi lr timestop gnome at ako village -- " +
+			"Katumi adds <report> " +
+			"to the list of load reports for the current boot. If you find " +
+			"a rare, global load, or complete a quest or the like, report " +
+			"it along with a location so other players will know!")
+	case oper == "lrdel":
+		txt = append(txt, "BadSyntax: tell katumi lrdel <num> -- " +
+			"Example: tell katumi lrdel 3 -- " +
+			"Katumi removes the " +
+			"numbered item from the load reports, if a quest is completed " +
+			"or a rare killed, or a report found to be inaccurate. Please " +
+			"do not abuse this command - this service helps everyone.")
+	case oper == "stat":
+		txt = append(txt, "BadSyntax: tell katumi stat <item> -- " +
+			"Example: tell katumi stat isha cloak -- " +
+			"Katumi provides stat info for the item named. " +
+			"Use 'astat' for full text of acronyms and keywords. " +
+			"The name search is fairly forgiving. Please send new stats " +
+			"in an mwrite to katumi or email to kristi.michaels@gmail.com")
+	case oper == "astat":
+		txt = append(txt, "BadSyntax: tell katumi astat <item> -- " +
+			"Example tell katumi astat destruction sword -- " +
+			"Katumi provides full " +
+			"stat information for the item named. Use 'stat' for short " +
+			"text. The name search is fairly forgiving, though the stats " +
+			"are a little buggy right now since I haven't put much time " +
+			"into it.")
+	case oper == "fstat":
+		txt = append(txt, "BadSyntax: tell katumi fstat <stat> <sym> <num>" +
+			"[, <stat2> <sym2> <num2>][, resist <resist>][, slot <slot>] -- " +
+			"Example: tell katumi fstat maxagi > 0, resist fire, slot ear -- " +
+			"Katumi provides up to 10 results which match the parameters.")
+		txt = append(txt, "Type attribs as they appear in stats: str, maxstr, svsp, " +
+			"dam, sf_illu, fire, unarm, ear, on_body, etc. " +
+			"Valid comparisons are >, <, and =. " +
+			"Resists check for a positive value. " +
+			"Other options will be added later.")
+	default:
+		txt = append(txt, NotFound("help file", oper))
+	}
+	return txt
+}
+
+func Fstat(oper string) []string {
+	var txts []string
+	opers := strings.Split(oper, ", ")
+	query := "SELECT short_stats FROM items"
+	var args []interface{}
+	for _, ops := range opers {
+		fop := strings.Fields(ops)
+		switch {
+		case len(fop) == 3:
+			fop[0] = strings.ToLower(fop[0])
+			comp := fop[1]
+			if strings.ContainsAny(comp, "=<>") {
+				if !strings.Contains(query, "WHERE") {
+					query += " WHERE item_id IN"
+				} else {
+					query += " AND item_id IN"
+				}
+				query += " (SELECT i.item_id FROM items i, item_attribs a " +
+					"WHERE i.item_id = a.item_id " +
+					"AND attrib_abbr = ? AND attrib_value " + comp + " ?)"
+				args = append(args, fop[0], fop[2])
+			}
+		case len(fop) == 2:
+			if strings.ToLower(fop[0]) == "resist" {
+				res := strings.ToLower(fop[1])
+				if !strings.Contains(query, "WHERE") {
+					query += " WHERE item_id IN"
+				} else {
+					query += " AND item_id IN"
+				}
+				query += " (SELECT i.item_id FROM items i, item_resists r " +
+					"WHERE i.item_id = r.item_id " +
+					"AND resist_abbr = ? AND resist_value > 0)"
+				args = append(args, res)
+			} else if strings.ToLower(fop[0]) == "slot" {
+				slot := "%" + strings.ToLower(fop[1]) + "%"
+				if !strings.Contains(query, "WHERE") {
+					query += " WHERE item_id IN"
+				} else {
+					query += " AND item_id IN"
+				}
+				query += " (SELECT i.item_id FROM items i, item_slots s " +
+					"WHERE i.item_id = s.item_id " +
+					"AND slot_abbr LIKE ?)"
+				args = append(args, slot)
+			}
+		}
+	}
+	query += " LIMIT 10;"
+	if strings.Contains(query, "WHERE") {
+		db := OpenDB()
+		defer db.Close()
+
+		// debugging
+		//log.Printf("Query : %s\n", query)
+		//log.Printf("Params: %s\n", args)
+
+		stmt, err := db.Prepare(query)
+		ChkErr(err)
+		defer stmt.Close()
+
+		rows, err := stmt.Query(args...)
+		ChkErr(err)
+		defer rows.Close()
+
+		var replied bool
+		for rows.Next() {
+			var stats string
+			err = rows.Scan(&stats)
+			txts = append(txts, stats)
+			replied = true
+		}
+		if !replied {
+			txts = append(txts, NotFound("item(s)", oper))
+		}
+		ChkRows(rows)
+	} else {
+		txts = append(txts, BadSyntax)
+	}
+	return txts
+}
+
+func Who(oper string) string {
+	db := OpenDB()
+	defer db.Close()
+
+	var txt string
+	query := "SELECT account_name, char_name " +
+		"FROM chars WHERE vis = 't' " +
+		"AND (account_name = " +
+		"(SELECT account_name FROM chars " +
+		"WHERE LOWER(char_name) = LOWER(?) AND vis = 't') " +
+		"OR LOWER(account_name) = LOWER(?)) " +
+		"ORDER BY char_level DESC, char_name ASC"
+
+	rows, err := db.Query(query, oper, oper)
+	ChkErr(err)
+	defer rows.Close()
+
+	for rows.Next() {
+		err = rows.Scan(&Char.acct, &Char.name)
+		if strings.Contains(txt, "@") {
+			txt += ", " + Char.name
+		} else {
+			txt = "@" + Char.acct
+			txt += ": " + Char.name
+		}
+	}
+	ChkRows(rows)
+
+	if !strings.Contains(txt, "@") {
+		txt = NotFound("character or account", oper)
+	}
+	return txt
+}
+
+var BadSyntax string = "Invalid syntax. For valid syntax: tell katumi ?, " +
+	"tell katumi help <cmd>"
+
 func ReplyTo(char string, tell string) {
 	info := "I am a Helper Bot (Beta). " +
 		"Valid commands: ?, help <cmd>, hidden?, " +
@@ -110,9 +341,7 @@ func ReplyTo(char string, tell string) {
 		"stat <item>, astat <item>, fstat <att> <comp> <val>. " +
 		"For further information, tell katumi help <cmd>"
 	// By default, replies will use 'invalid syntax', requiring reassignment
-	syntax := "Invalid syntax. For valid syntax: tell katumi ?, " +
-		"tell katumi help <cmd>"
-	txt := syntax
+	txt := BadSyntax
 
 	oper := ""
 	split := strings.SplitN(tell, " ", 2)
@@ -129,130 +358,13 @@ func ReplyTo(char string, tell string) {
 	case cmd == "?":
 		Reply(char, info)
 	case cmd == "help":
-		switch {
-		case oper == "":
+		if oper == "" {
 			Reply(char, info)
-		case oper == "?":
-			txt = "Syntax: tell katumi ? -- " +
-				"Katumi provides a full listing of valid commands."
-			Reply(char, txt)
-		case oper == "hidden?":
-			txt = "Syntax: tell katumi hidden? -- " +
-				"Katumi sends a tell in reply " +
-				"if she can see you. If you receive no reply, you are hidden. " +
-				"Katumi has permanent detect invis to ensure that won't cause " +
-				"issues."
-			Reply(char, txt)
-		case oper == "who":
-			txt = "Syntax: tell katumi who <acct/char> -- " +
-				"Example: tell katumi who rynshana -- " +
-				"Katumi provides the account name along with a list of every " +
-				"known alt of the named character as a reply. Also works with " +
-				"account names."
-			Reply(char, txt)
-		case oper == "char":
-			txt = "Syntax: tell katumi char <char> -- " +
-				"Example: tell katumi char rynshana -- " +
-				"Katumi provides the account name along with full information " +
-				"on the named character as a reply, " +
-				"to include level, class, race, and date/time last seen."
-			Reply(char, txt)
-		case oper == "find":
-			txt = "Syntax: tell katumi find <acct/char> -- " +
-				"Example: tell katumi find rynshana -- " +
-				"Katumi provides the account name along with the last known " +
-				"sighting of any of that character's alts. " +
-				"If they have an alt online, " +
-				"the time will measure in seconds. " +
-				"Also works with account names."
-			Reply(char, txt)
-		case oper == "clist":
-			txt = "Syntax: tell katumi clist <acct/char> -- " +
-				"Example: tell katumi clist rynshana -- " +
-				"Katumi provides a full " +
-				"listing of every known alt belonging to <char>, including race, " +
-				"class, level, and date/time last seen, matching the format of " +
-				"the 'char' command. Also works with account names."
-			Reply(char, txt)
-		case oper == "class":
-			txt = "Syntax: tell katumi class <class> -- " +
-				"Example: tell katumi class enchanter -- " +
-				"Katumi provides a " +
-				"list of alts matching the named class for characters who " +
-				"are currently online, letting group leaders find useful " +
-				"alts from the 'who' list."
-			Reply(char, txt)
-		case oper == "delalt":
-			txt = "Syntax: tell katumi delalt <char> -- " +
-				"Example: tell katumi delalt rynshana -- " +
-				"Katumi no longer " +
-				"provides information on the alt, removing it from 'clist', " +
-				"'who', and 'find' commands. Only works for characters " +
-				"attached to the same account requesting the removal."
-			Reply(char, txt)
-		case oper == "addalt":
-			txt = "Syntax: tell katumi addalt <char> -- " +
-				"Example: tell katumi addalt rynshana -- " +
-				"Katumi begins " +
-				"providing information on the named alt, who had previously " +
-				"been removed with 'delalt', adding the character back to " +
-				"'clist', 'who', and 'find' commands. Only works for chars " +
-				"attached to the same account."
-			Reply(char, txt)
-		case oper == "lr":
-			txt = "Syntax: tell katumi lr -- " +
-				"Katumi provides a list of load " +
-				"reports for the current boot. This could be rares or quests " +
-				"other players have found or completed. Use the 'lrdel' command " +
-				"to remove bad or out of date reports."
-			Reply(char, txt)
-			txt = "Syntax: tell katumi lr <report> -- " +
-				"Example: tell katumi lr timestop gnome at ako village -- " +
-				"Katumi adds <report> " +
-				"to the list of load reports for the current boot. If you find " +
-				"a rare, global load, or complete a quest or the like, report " +
-				"it along with a location so other players will know!"
-			Reply(char, txt)
-		case oper == "lrdel":
-			txt = "Syntax: tell katumi lrdel <num> -- " +
-				"Example: tell katumi lrdel 3 -- " +
-				"Katumi removes the " +
-				"numbered item from the load reports, if a quest is completed " +
-				"or a rare killed, or a report found to be inaccurate. Please " +
-				"do not abuse this command - this service helps everyone."
-			Reply(char, txt)
-		case oper == "stat":
-			txt = "Syntax: tell katumi stat <item> -- " +
-				"Example: tell katumi stat isha cloak -- " +
-				"Katumi provides stat info for the item named. " +
-				"Use 'astat' for full text of acronyms and keywords. " +
-				"The name search is fairly forgiving. Please send new stats " +
-				"in an mwrite to katumi or email to kristi.michaels@gmail.com"
-			Reply(char, txt)
-		case oper == "astat":
-			txt = "Syntax: tell katumi astat <item> -- " +
-				"Example tell katumi astat destruction sword -- " +
-				"Katumi provides full " +
-				"stat information for the item named. Use 'stat' for short " +
-				"text. The name search is fairly forgiving, though the stats " +
-				"are a little buggy right now since I haven't put much time " +
-				"into it."
-			Reply(char, txt)
-		case oper == "fstat":
-			txt = "Syntax: tell katumi fstat <stat> <sym> <num>" +
-				"[, <stat2> <sym2> <num2>][, resist <resist>][, slot <slot>] -- " +
-				"Example: tell katumi fstat maxagi > 0, resist fire, slot ear -- " +
-				"Katumi provides up to 10 results which match the parameters."
-			Reply(char, txt)
-			txt = "Type attribs as they appear in stats: str, maxstr, svsp, " +
-				"dam, sf_illu, fire, unarm, ear, on_body, etc. " +
-				"Valid comparisons are >, <, and =. " +
-				"Resists check for a positive value. " +
-				"Other options will be added later."
-			Reply(char, txt)
-		default:
-			txt = NotFound("help file", oper)
-			Reply(char, txt)
+		} else {
+			txts := Help(oper)
+			for _, txt = range txts {
+				Reply(char, txt)
+			}
 		}
 	case cmd == "stat" && oper != "":
 		txt = FindItem(oper, "short_stats")
@@ -261,117 +373,13 @@ func ReplyTo(char string, tell string) {
 		txt = FindItem(oper, "long_stats")
 		Reply(char, txt)
 	case cmd == "fstat" && oper != "":
-		opers := strings.Split(oper, ", ")
-		query := "SELECT short_stats FROM items"
-		var args []interface{}
-		for _, ops := range opers {
-			fop := strings.Fields(ops)
-			switch {
-			case len(fop) == 3:
-				fop[0] = strings.ToLower(fop[0])
-				comp := fop[1]
-				if strings.ContainsAny(comp, "=<>") {
-					if !strings.Contains(query, "WHERE") {
-						query += " WHERE item_id IN"
-					} else {
-						query += " AND item_id IN"
-					}
-					query += " (SELECT i.item_id FROM items i, item_attribs a " +
-						"WHERE i.item_id = a.item_id " +
-						"AND attrib_abbr = ? AND attrib_value " + comp + " ?)"
-					args = append(args, fop[0], fop[2])
-				}
-			case len(fop) == 2:
-				if strings.ToLower(fop[0]) == "resist" {
-					res := strings.ToLower(fop[1])
-					if !strings.Contains(query, "WHERE") {
-						query += " WHERE item_id IN"
-					} else {
-						query += " AND item_id IN"
-					}
-					query += " (SELECT i.item_id FROM items i, item_resists r " +
-						"WHERE i.item_id = r.item_id " +
-						"AND resist_abbr = ? AND resist_value > 0)"
-					args = append(args, res)
-				} else if strings.ToLower(fop[0]) == "slot" {
-					slot := "%" + strings.ToLower(fop[1]) + "%"
-					if !strings.Contains(query, "WHERE") {
-						query += " WHERE item_id IN"
-					} else {
-						query += " AND item_id IN"
-					}
-					query += " (SELECT i.item_id FROM items i, item_slots s " +
-						"WHERE i.item_id = s.item_id " +
-						"AND slot_abbr LIKE ?)"
-					args = append(args, slot)
-				}
-			}
-		}
-		query += " LIMIT 10;"
-		if strings.Contains(query, "WHERE") {
-			db := OpenDB()
-			defer db.Close()
-
-			// debugging
-			//log.Printf("Query : %s\n", query)
-			//log.Printf("Params: %s\n", args)
-
-			stmt, err := db.Prepare(query)
-			ChkErr(err)
-			defer stmt.Close()
-
-			rows, err := stmt.Query(args...)
-			ChkErr(err)
-			defer rows.Close()
-
-			var replied bool
-			for rows.Next() {
-				var stats string
-				err = rows.Scan(&stats)
-				Reply(char, stats)
-				replied = true
-			}
-			if !replied {
-				txt = NotFound("item(s)", oper)
-				Reply(char, txt)
-			}
-			ChkRows(rows)
-		} else {
-			Reply(char, syntax)
+		txts := Fstat(oper)
+		for _, txt = range txts {
+			Reply(char, txt)
 		}
 	case cmd == "who" && oper != "":
-		db := OpenDB()
-		defer db.Close()
-
-		query := "SELECT account_name, char_name " +
-			"FROM chars WHERE vis = 't' " +
-			"AND (account_name = " +
-			"(SELECT account_name FROM chars " +
-			"WHERE LOWER(char_name) = LOWER(?) AND vis = 't') " +
-			"OR LOWER(account_name) = LOWER(?)) " +
-			"ORDER BY char_level DESC, char_name ASC"
-
-		rows, err := db.Query(query, oper, oper)
-		ChkErr(err)
-		defer rows.Close()
-
-		for rows.Next() {
-			err = rows.Scan(&Char.acct, &Char.name)
-			if strings.Contains(txt, "@") {
-				txt += ", " + Char.name
-			} else {
-				txt = "@" + Char.acct
-				txt += ": " + Char.name
-			}
-		}
-		ChkRows(rows)
-
-		if strings.Contains(txt, "@") {
-			Reply(char, txt)
-		} else {
-			txt = NotFound("character or account", oper)
-			Reply(char, txt)
-		}
+		txt = Who(oper)
+		Reply(char, txt)
 	case cmd == "clist" && oper != "":
 		db := OpenDB()
 		defer db.Close()
@@ -653,7 +661,7 @@ func ReplyTo(char string, tell string) {
 			txt = fmt.Sprintf("Load reported: %s", oper)
 			Reply(char, txt)
 		} else {
-			Reply(char, syntax)
+			Reply(char, BadSyntax)
 		}
 	case cmd == "lrdel" && oper != "":
 		db := OpenDB()
@@ -669,7 +677,7 @@ func ReplyTo(char string, tell string) {
 
 		num, err := strconv.Atoi(oper)
 		if err != nil {
-			Reply(char, syntax)
+			Reply(char, BadSyntax)
 			return
 		}
 		var curboot string
@@ -709,6 +717,6 @@ func ReplyTo(char string, tell string) {
 			Reply(char, txt)
 		}
 	default:
-		Reply(char, syntax)
+		Reply(char, BadSyntax)
 	}
 }

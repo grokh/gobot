1f5dc4e5eb403ee3ce260c5a80c79ac3de9f6e66
Scott Dickey <scott@grokh.com>
2013-04-25 22:06:20 -0400

    Added more unit tests, moved test logging to a file

--- tell_test.go
+++ tell_test.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"testing"
+	"log"
+	"os"
 )
 
 func Test_ReplyTo(t *testing.T) {
+	f, err := os.OpenFile("logs/test.log", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0640)
+	defer f.Close()
+	ChkErr(err)
+	log.SetOutput(f)
+
 	txt := ReplyTo("Yog", "blah")
 	good := "Invalid syntax. For valid syntax: tell katumi ?, "+
 		"tell katumi help <cmd>"
 	if txt[0] != good {
-		t.Error("'Invalid Syntax' check failed.", txt[0])
+		t.Error("ReplyTo Check failed: Yog tells you 'blah'", txt[0])
 	}
 	
 	txt = ReplyTo("Yog", "?")
@@ -19,7 +26,7 @@ func Test_ReplyTo(t *testing.T) {
 		"lr, lr <report>, stat <item>, astat <item>, fstat <att> <comp> <val>. "+
 		"For further information, tell katumi help <cmd>"
 	if txt[0] != good {
-		t.Error("'Info (?)' check failed.", txt[0])
+		t.Error("ReplyTo Check failed: Yog tells you '?'", txt[0])
 	}
 	
 	txt = ReplyTo("Yog", "stat bane stiletto")
@@ -29,6 +36,41 @@ func Test_ReplyTo(t *testing.T) {
 		"'Dragonstrike' effect: instant kill, 5 charge * Float Magic No_Burn No_Loc !Fighter "+
 		"!Mage !Priest * Wt:5 Val:0 * Zone: Tiamat (R) * Last ID: 2006-01-16"
 	if txt[0] != good {
-		t.Error("'stat bane stiletto' check failed.", txt[0])
+		t.Error("ReplyTo Check failed: Yog tells you 'stat bane stiletto'", txt[0])
+	}
+
+	txt = ReplyTo("Yog", "hidden")
+	good = "Yog is NOT hidden!"
+	if txt[0] != good {
+		t.Error("ReplyTo Check failed: Yog tells you 'hidden'", txt[0])
+	}
+
+	txt = ReplyTo("Someone", "hidden")
+	if len(txt) > 0 {
+		t.Error("ReplyTo Check failed: Someone tells you 'hidden'", txt[0])
+	}
+
+	txt = ReplyTo("Yog", "who bob")
+	good = "404 character or account not found: bob"
+	if txt[0] != good {
+		t.Error("ReplyTo Check failed: Yog tells you 'who bob'", txt[0])
+	}
+
+	txt = ReplyTo("Yog", "who bonble")
+	good = "@Nyyrazzilyss: Nyyrazzilyss, Bonble"
+	if txt[0] != good {
+		t.Error("ReplyTo Check failed: Yog tells you 'who bonble'", txt[0])
+	}
+
+	txt = ReplyTo("Yog", "char Bob")
+	good = "404 character not found: Bob"
+	if txt[0] != good {
+		t.Error("ReplyTo Check failed: Yog tells you 'char Bob'", txt[0])
+	}
+
+	txt = ReplyTo("Yog", "char bonble")
+	good = "[5 Bard] Bonble (Halfling) (@Nyyrazzilyss) seen 2013-04-13 10:00:24"
+	if txt[0] != good {
+		t.Error("ReplyTo Check failed: Yog tells you 'char bonble'", txt[0])
 	}
 }

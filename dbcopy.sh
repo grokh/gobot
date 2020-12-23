507f304846cdefc81a73d20b192075a1da296fc4
Scott Dickey <scott@grokh.com>
2020-12-23 20:29:21 +0000

    added updating of the gobot dbs to dbcopy

--- dbcopy.sh
+++ dbcopy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cp /srv/bot/toril.db /tmp/
-sqlite3 /tmp/toril.db "delete from chars where vis = 'f';"
-cp /tmp/toril.db /srv/web/grokh.com/html/
+cp /srv/bot/toril.db /tmp/toril.db
+sqlite3 /tmp/toril.db "DELETE FROM chars WHERE vis = 'f';"
+cp /tmp/toril.db /srv/web/grokh.com/html/toril.db
+cp /tmp/toril.db /home/trance/go/src/github.com/grokh/gobot/toril.db
+cp /tmp/toril.db /home/trance/go/src/github.com/grokh/gobot/toril.db.bak

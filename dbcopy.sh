6bd09e527b1dfac2b38f4155a8d8a8d3fff4d347
Scott Dickey <scott@grokh.com>
2020-12-25 01:36:50 +0000

    removed additional db copy that i dont need any more

--- dbcopy.sh
+++ dbcopy.sh
@@ -4,4 +4,3 @@ cp /srv/bot/toril.db /tmp/toril.db
 sqlite3 /tmp/toril.db "DELETE FROM chars WHERE vis = 'f';"
 cp /tmp/toril.db /srv/web/grokh.com/html/toril.db
 cp /tmp/toril.db /home/trance/go/src/github.com/grokh/gobot/toril.db
-cp /tmp/toril.db /home/trance/go/src/github.com/grokh/gobot/toril.db.bak

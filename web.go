518c05630fd83854d8adae49f90159bda1dd72f0
Scott Dickey <scott@grokh.com>
2020-12-29 01:21:42 +0000

    Moved index to static title and dynamic date

--- web.go
+++ web.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
+	"log"
 	"net/http"
 )
 
 type Page struct {
-	Title   string
 	Date    string
 	Results []string
 }
@@ -14,6 +15,25 @@ type Page struct {
 var templates = template.Must(template.ParseFiles(
 	"html/index.html",
 ))
+
+func mostRecent() string {
+	db := OpenDB()
+    defer db.Close()
+
+	query := "SELECT MAX(last_id) FROM items"
+	stmt, err := db.Prepare(query)
+    ChkErr(err)
+    defer stmt.Close()
+
+	var txt string
+	err = stmt.QueryRow().Scan(&txt)
+	if err == sql.ErrNoRows {
+		txt = "Database Lost"
+	} else if err != nil {
+		log.Fatal(err)
+	}
+	return txt
+}
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	err := templates.ExecuteTemplate(w, tmpl, p)
@@ -23,8 +43,7 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 }
 
 func eqHandler(w http.ResponseWriter, r *http.Request) {
-	p := &Page{Title: "title", Date: "2020-12-27"}
-	p.Title = "TorilMUD Equipment Database"
+	p := &Page{Date: mostRecent()}
 	if r.Method == "POST" {
 		results := FindItem(r.PostFormValue("itemName"), "short_stats")
 		p.Results = results

867530eebc169b6a250e622582328fe2e8568b1c
Scott Dickey <scott@grokh.com>
2020-12-31 00:15:06 +0000

    Added all of the advanced and list search features to the index using web.go to populate fields from the database. Spent a lot of time trying to figure out how to toggle display features.

--- html/form.js
+++ html/form.js
@@ -1,7 +1,7 @@
 function selectItem (selectedItem) {
 	document.searchForm.itemName.value = selectedItem;
 	document.searchForm.submit.click();
-}
+};
 
 function toggleLayer (whichLayer) {
 	var elem, vis;
@@ -9,14 +9,7 @@ function toggleLayer (whichLayer) {
 	if (document.getElementById) // this is the way the standards work
 		elem = document.getElementById(whichLayer);
 	
-	else if (document.all) // this is the way old msie versions work
-		elem = document.all[whichLayer];
-	
 	vis = elem.style;
 	
-	// if the style.display value is blank we try to figure it out here
-	if (vis.display==''&&elem.offsetWidth!=undefined&&elem.offsetHeight!=undefined)
-		vis.display = (elem.offsetWidth!=0&&elem.offsetHeight!=0)?'block':'none';
-	
 	vis.display = (vis.display==''||vis.display=='block')?'none':'block';
-}
+};
--- web.go
+++ web.go
@@ -1,34 +1,188 @@
 package main
 
 import (
-	"database/sql"
+//	"database/sql"
 	"html/template"
-	"log"
+//	"log"
 	"net/http"
 )
 
-type Page struct {
-	Date string
-	Res  []string
+type Attrib struct {
+	AttAbbr string
+	AttDisp string
 }
 
-func mostRecent() string {
+type Slot struct {
+	SlotAbbr string
+	SlotDisp string
+}
+
+type IType struct {
+	TypeAbbr string
+	TypeDisp string
+}
+
+type Effect struct {
+	EffAbbr string
+	EffDisp string
+}
+
+type Resist struct {
+	ResAbbr string
+	ResDisp string
+}
+
+type Flag struct {
+	FlagAbbr string
+	FlagDisp string
+}
+
+type Supp struct {
+	SuppAbbr string
+	SuppDisp string
+}
+
+type Page struct {
+	Date    string
+	Results []string
+	Attribs []Attrib
+	Slots   []Slot
+	Types   []IType
+	Effects []Effect
+	Resists []Resist
+	Flags   []Flag
+	Supps   []Supp
+}
+
+func fillStructs() Page {
+	p := Page{
+        Date: "2020-12-29",
+    }
+
 	db := OpenDB()
     defer db.Close()
 
 	query := "SELECT MAX(last_id) FROM items"
-	stmt, err := db.Prepare(query)
+    stmt, err := db.Prepare(query)
     ChkErr(err)
     defer stmt.Close()
 
-	var txt string
-	err = stmt.QueryRow().Scan(&txt)
-	if err == sql.ErrNoRows {
-		txt = "Database Lost"
-	} else if err != nil {
-		log.Fatal(err)
+    err = stmt.QueryRow().Scan(&p.Date)
+	ChkErr(err)
+
+    query = "SELECT attrib_abbr, attrib_display FROM attribs"
+	stmt, err = db.Prepare(query)
+    ChkErr(err)
+    defer stmt.Close()
+
+	rows, err := stmt.Query()
+    ChkErr(err)
+    defer rows.Close()
+
+	for rows.Next() {
+		var a Attrib
+		err = rows.Scan(&a.AttAbbr, &a.AttDisp)
+		ChkErr(err)
+		p.Attribs = append(p.Attribs, a)
 	}
-	return txt
+
+	query = "SELECT slot_abbr, slot_display FROM slots"
+	stmt, err = db.Prepare(query)
+    ChkErr(err)
+    defer stmt.Close()
+
+    rows, err = stmt.Query()
+    ChkErr(err)
+    defer rows.Close()
+
+    for rows.Next() {
+		var s Slot
+		err = rows.Scan(&s.SlotAbbr, &s.SlotDisp)
+		ChkErr(err)
+		p.Slots = append(p.Slots, s)
+	}
+
+	query = "SELECT type_abbr, type_display FROM item_types"
+	stmt, err = db.Prepare(query)
+    ChkErr(err)
+    defer stmt.Close()
+
+    rows, err = stmt.Query()
+    ChkErr(err)
+    defer rows.Close()
+
+    for rows.Next() {
+		var t IType
+		err = rows.Scan(&t.TypeAbbr, &t.TypeDisp)
+		ChkErr(err)
+		p.Types = append(p.Types, t)
+	}
+
+	query = "SELECT effect_abbr, effect_display FROM effects"
+    stmt, err = db.Prepare(query)
+    ChkErr(err)
+    defer stmt.Close()
+
+    rows, err = stmt.Query()
+    ChkErr(err)
+    defer rows.Close()
+
+    for rows.Next() {
+        var e Effect
+        err = rows.Scan(&e.EffAbbr, &e.EffDisp)
+        ChkErr(err)
+        p.Effects = append(p.Effects, e)
+    }
+
+	query = "SELECT resist_abbr, resist_display FROM resists"
+    stmt, err = db.Prepare(query)
+    ChkErr(err)
+    defer stmt.Close()
+
+    rows, err = stmt.Query()
+    ChkErr(err)
+    defer rows.Close()
+
+    for rows.Next() {
+        var r Resist
+        err = rows.Scan(&r.ResAbbr, &r.ResDisp)
+        ChkErr(err)
+        p.Resists = append(p.Resists, r)
+    }
+
+	query = "SELECT flag_abbr, flag_display FROM flags"
+    stmt, err = db.Prepare(query)
+    ChkErr(err)
+    defer stmt.Close()
+
+    rows, err = stmt.Query()
+    ChkErr(err)
+    defer rows.Close()
+
+    for rows.Next() {
+        var f Flag
+        err = rows.Scan(&f.FlagAbbr, &f.FlagDisp)
+        ChkErr(err)
+        p.Flags = append(p.Flags, f)
+    }
+
+	query = "SELECT supp_abbr, supp_display FROM supps"
+    stmt, err = db.Prepare(query)
+    ChkErr(err)
+    defer stmt.Close()
+
+    rows, err = stmt.Query()
+    ChkErr(err)
+    defer rows.Close()
+
+    for rows.Next() {
+        var s Supp
+        err = rows.Scan(&s.SuppAbbr, &s.SuppDisp)
+        ChkErr(err)
+        p.Supps = append(p.Supps, s)
+    }
+
+	return p
 }
 
 var tmpl = template.Must(template.ParseFiles(
@@ -36,15 +190,13 @@ var tmpl = template.Must(template.ParseFiles(
 ))
 
 func eqHandler(w http.ResponseWriter, r *http.Request) {
-	p := Page{
-		Date: mostRecent(),
-		Res: nil,
-	}
+	p := fillStructs()
 	if r.Method == "POST" {
-		p.Res = FindItem(r.PostFormValue("itemName"), "short_stats")
+		p.Results = FindItem(r.PostFormValue("itemName"), "short_stats")
 	}
 	err := tmpl.Execute(w, p)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
     }
 }
+

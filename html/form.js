5a5bd6241c5bff4047b3c24e2082a114622aa27a
Scott Dickey <scott@grokh.com>
2021-01-02 00:55:57 +0000

    Changed to only support newest standards, re-added identification bit for current vis

--- html/form.js
+++ html/form.js
@@ -6,10 +6,13 @@ function selectItem (selectedItem) {
 function toggleLayer (whichLayer) {
 	var elem, vis;
 	
-	if (document.getElementById) // this is the way the standards work
-		elem = document.getElementById(whichLayer);
+	elem = document.getElementById(whichLayer);
 	
 	vis = elem.style;
+
+	// if the style.display value is blank we try to figure it out here
+	if (vis.display==''&&elem.offsetWidth!=undefined&&elem.offsetHeight!=undefined)
+		vis.display = (elem.offsetWidth!=0&&elem.offsetHeight!=0)?'block':'none';
 	
 	vis.display = (vis.display==''||vis.display=='block')?'none':'block';
 };

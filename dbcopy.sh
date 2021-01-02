4608416478c6c3fd4db2ffaa991c14c54aee763e
Scott Dickey <scott@grokh.com>
2021-01-02 15:48:22 +0000

    Updated torileq service to run from /srv/web instead of /home folder. Updated dbcopy to push db to new location. Added publish command to move from development area to running site.

--- dbcopy.sh
+++ dbcopy.sh
@@ -5,3 +5,4 @@ sqlite3 /tmp/toril.db "DELETE FROM chars WHERE vis = 'f';"
 cp /tmp/toril.db /srv/web/grokh.com/html/toril.db
 cp /tmp/toril.db /home/trance/go/src/github.com/grokh/gobot/toril.db
 cp /tmp/toril.db /home/trance/mud/torilmud/toril.db
+cp /tmp/toril.db /srv/web/torileq/toril.db
---
+++ publish.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp /srv/bot/toril.db /srv/web/torileq/toril.db
+cp ~/go/bin/gobot /srv/web/torileq/
+rsync -azv ~/go/src/github.com/grokh/gobot/html/ /srv/web/torileq/html/

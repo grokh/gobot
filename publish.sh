aa338afeb2d7f12a77278a794092a8af2ad759a4
Scott Dickey <scott@grokh.com>
2021-01-09 16:57:46 +0000

    added stop/start commands for torileq site to ensure publishing

--- publish.sh
+++ publish.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo systemctl stop torileq
 cp /srv/bot/toril.db /srv/web/torileq/toril.db
 cp ~/go/bin/gobot /srv/web/torileq/
 rsync -azv ~/go/src/github.com/grokh/gobot/html/ /srv/web/torileq/html/
+sudo systemctl start torileq
